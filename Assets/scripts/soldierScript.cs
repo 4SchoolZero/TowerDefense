d409a82b0eb5b557d02fc0c9646b809d95ca8b5f
Tijmen <34179@ma-web.nl>
2022-09-23 16:16:48 +0200

    tower mechanic, more assets, fixed animations

--- Assets/scripts/soldierScript.cs
+++ Assets/scripts/soldierScript.cs
@@ -4,40 +4,35 @@ using UnityEngine;
 
 public class soldierScript : MonoBehaviour
 {
-    [SerializeField]private Transform target; 
+    [SerializeField]private GameObject target;
     [SerializeField]private Animator anim;
+    [SerializeField]public GameObject pow;
+
     int healthZombie = 100;
-    
+     public void dmg(){
+        healthZombie = healthZombie - 1;
+        if(healthZombie <= 0){
+            Destroy(target);
+            pow.SetActive(false);
+        }
+    }
+    public void track(){
+        var y = target.transform;
+        float distance = Vector3.Distance(y.position, transform.position);
+        if(distance < 13){
+            transform.LookAt(y);
+            anim.Play("shoot");
+            pow.SetActive(true);
+            InvokeRepeating("dmg", 4f, 2f);
+        }
+    }
     void Start()
     {
-        StartCoroutine(test());
         anim = GetComponent<Animator>();
         anim.Play("idle");
     }
-    public void dmg(int dmg){
-        var x = GameObject.FindWithTag("Zombie");
-        healthZombie = healthZombie - dmg;
-        anim.Play("shoot");
-        if(healthZombie <= 0){
-            Destroy(x);
-        }
-    }
-    public void shoot(){
-        float distance = Vector3.Distance(target.position, transform.position);
-        if(distance < 10){
-            transform.LookAt(target);
-        }
-        else{
-            anim.Play("idle");
-        }
-    }
-    IEnumerator test(){
-        shoot();
-        yield return new WaitForSeconds(1);
-        dmg(20);
-    }
     void Update()
     {
-        
+        track();
     }
 }
