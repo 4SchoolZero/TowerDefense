f9d809f8bacc264c30374e762d8afebcd50347b6
Tijmen <34179@ma-web.nl>
2022-10-26 14:02:38 +0200

    powerup edit

--- Assets/scripts/powerUps.cs
+++ Assets/scripts/powerUps.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class powerUps : MonoBehaviour
 {
+    public GameObject muzzle;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +16,9 @@ public class powerUps : MonoBehaviour
     }
     public void turret(){
         GameObject[] targets = GameObject.FindGameObjectsWithTag("Zombie"); 
+        muzzle.SetActive(true);
         for (var i = 0; i < targets.Length; i++) {
-            Destroy(targets[i], 3);
+            Destroy(targets[i].gameObject, 3);
         }
     }
     public void nuke(){
